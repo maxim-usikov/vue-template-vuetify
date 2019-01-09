c85cd3fc223d4294e0b64a71bd070f4479b0218c
Maxim Usikov <mu.usikov@gmail.com>
2019-01-09 17:51:21 +0300

    chore(semantic-release): try production config

--- release.config.js
+++ release.config.js
@@ -1,15 +1,24 @@
 module.exports = {
-  branch: 'wip', // todo: change to default master
+  branch: 'master',
   tagFormat: 'v${version}',
+  dryRun: false,
+  ci: false,
+  debug: false,
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+
     // NOTE: DO NOT RELEASE ON NPM !!!
     // '@semantic-release/npm',
+
+    // Release to gitlab.
     '@semantic-release/gitlab',
+
+    // Release to github.
+    // '@semantic-release/github',
+
+    // Release to git.
+    // '@semantic-release/git',
   ],
-  dryRun: true,
-  ci: false,
-  debug: false,
 };
